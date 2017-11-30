ee57ae08a6df4f6e1050bd6ae8d6ccd499f87f05
rstar2 <rstar2@abv.bg>
2017-11-30 16:47:09 +0200

    Finished

--- index.js
+++ index.js
@@ -6,11 +6,7 @@
 // This will also work on Windows, as npm will helpfully
 // install a cmd wrapper alongside your script.
 
-/* eslint-disable no-console */
-
 const chalk = require('chalk');
-const minimist = require('minimist');
-
 
 const guiStart = require('./lib/gui').start;
 const utils = require('./lib/utils');
@@ -27,34 +23,28 @@ if (utils.isDirExists('.gitX')) {
 }
 
 
+/**
+ * @returns {{name:String, description?:String}}
+ */
 function getRepoParams() {
     // Usage:
-    // $ ginit name "Long Description"
-    // get the main arguments (under the '_' param)
-    let argv = minimist(process.argv.slice(2));
-    argv = argv._;
+    // $ ginit name "Long Description" --no-ssh
+
+    let argv = require('minimist')(process.argv.slice(2));
 
     // get the name of the current working directory
-    let name = utils.getNameCWD();
-    let description;
-
-    if (argv.length > 0) {
-        name = argv[0];
-
-        if (argv.length > 1) {
-            description = argv[1];
-        }
-    }
-
-    return { name, description };
+    const name = argv._[0] || utils.getNameCWD();
+    const description = argv._[1];
+    const ssh = argv['ssh'];
+    return { name, description, ssh };
 }
 
 // get user's GitHub credentials and the "new" Repository parameters
-Promise.all([github.getToken(), getRepoParams()])
-    .then(([token, repo]) => {
-        console.log(`Token : ${token}`);
-        console.log(`Repo : ${repo.name} - ${repo.description}`);
-    })
+github.authenticate()
+    .then(() => getRepoParams())
+    .then(repoParams => github.createRepo(repoParams.name, repoParams.description, repoParams.ssh))
+    .then(repoUrl => git.createGitignore().then(() => git.setup(repoUrl)))
+    .then(() => console.log(chalk.blue('Success!')))
     .catch(err => {
         console.log(chalk.red(`Failed - ${err} !`));
         process.exit(1);
